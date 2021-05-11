5b3c73571a415eda8f9edcd4de5ba58455cf2308
Didio Perdomo <didioarbey@gmail.com>
2021-05-10 22:03:15 -0500

    sacar las entradas por consola y solo en codigo

--- Retos/reto1.py
+++ Retos/reto1.py
@@ -1,13 +1,13 @@
 #declaro la funcion de calculadora
-def calculadoraRectangulo(ancho,largo):
+def calculadoraRectangulo(ancho:float,largo:float):
     perimetro = (2 * ancho) +(2 * largo)
     area = ancho * largo
     return "El cuadrado tiene un perimetro de: " +str(perimetro) + " y un area de: "+str(area) #el mensaje retorna cuadrdo porque asi lo estipula el reto.
 
 #capturo por consola los valores del rectangulo y asu vez se fuerzan para que sean de tipo float
-ancho = float(input("Digite el valor del ancho del rectangulo "))
-largo = float(input("Digite el valor del largo del rectangulo "))
+#ancho = float(input("Digite el valor del ancho del rectangulo "))
+#largo = float(input("Digite el valor del largo del rectangulo "))
 
-mostrar=calculadoraRectangulo(ancho,largo)
+mostrar=calculadoraRectangulo(3,2.5)
 
 print(mostrar)
