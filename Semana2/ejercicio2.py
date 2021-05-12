b83755eea138a804aa1cfc7b60d5a97315133994
Didio Perdomo <didioarbey@gmail.com>
2021-05-12 08:03:19 -0500

    Ejercicios de condicion simple

---
+++ Semana2/ejercicio2.py
@@ -0,0 +1,11 @@
+
+def mayor(edad):
+    if edad >= 18:
+        print ("Usted es mayor de edad")
+    else:
+        print ("Usted es menor de edad")
+
+edad = int(input("Digite su edad "))
+
+print=(mayor(edad))
+
