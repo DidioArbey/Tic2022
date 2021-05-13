13d7a5c43ac9d716c53c89c56352c2d395bde309
Didio Perdomo <didioarbey@gmail.com>
2021-05-13 08:56:07 -0500

    ejemplos de string y como evaluarlos

---
+++ Semana2/string.py
@@ -0,0 +1,22 @@
+fruta = 'fresa'
+letra = fruta[0]
+#print(letra)
+#len para ver la longitud de un string
+#para obtener la ultima letra de un string se uda
+longitud = len(fruta)
+ultimo = fruta[longitud-1]
+print(ultimo)
+#seleccionar una parte
+nombre = "Hola mundo"
+print(nombre[0:6])
+#asignar una nueva letra en un estring
+salud ="¡hola mundo!"
+nuevoSaludo = "J"+salud[1:]
+print(nuevoSaludo)
+#operador
+"ban" in "banana"
+True
+#eliminar espacios
+linea="    holas care bolas    "
+linea.strip()
+#todo esto en la clase nueve de documentacion
