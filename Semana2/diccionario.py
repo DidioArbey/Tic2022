f34037e9d10abc0afd656d864c7780a5c660def8
Didio Perdomo <didioarbey@gmail.com>
2021-05-13 10:02:13 -0500

    diccionarios

---
+++ Semana2/diccionario.py
@@ -0,0 +1,9 @@
+def mostrarMoneda():
+    moneda = {'Euro':'€', 'Dollar':'USD', 'Peso':'$'}
+    tipo_moneda = input("Ingrese el tipo de moneda: ")
+    if tipo_moneda in moneda:
+        print(moneda[tipo_moneda])
+    else:
+        print("No esta la moneda")
+
+mostrarMoneda()
