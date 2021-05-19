61ab0b25a87878e5e398f4c9c99847a45d3cd5f8
Didio Perdomo <didioarbey@gmail.com>
2021-05-19 09:58:43 -0500

    ejercicios con listas

---
+++ Semana3/listasCompuestas.py
@@ -0,0 +1,4 @@
+notas=[[4,5],[6,9],[7,3]]#un arreglo de arreglos o una matriz
+
+for x in range(len(notas[0])):
+    print(notas[0][x])
---
+++ Semana3/listasParalelas.py
@@ -0,0 +1,14 @@
+nombres=[]
+edades=[]
+
+for x in range(5):
+    nom=input("Ingresar el nombre de la persona: ")
+    nombres.append(nom)
+    ed=int(input("ingrese la edad de dica persona: "))
+    edades.append(ed)
+
+print("Nombres de las personas mayores de edad: ")
+
+for x in range(5):
+    if edades[x]>=18:
+        print(nombres[x])
