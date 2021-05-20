47f107aa7c598190cedbe31c1cbbc3df79572dee
Didio Perdomo <didioarbey@gmail.com>
2021-05-20 00:20:21 -0500

    repaso python1

--- Retos/retovicky.py
+++
@@ -1,7 +0,0 @@
-def  calculadoraRectangulo(ancho: float, largo:float)->str:
-    perimetro = (ancho*2) + (largo*2)
-    area= (largo*ancho)
-    mostrar=("El cuadrado tiene un perimetro de:{0:.1f}".format(perimetro) + " y un área de:{0:.1f}".format(area))
-    print(mostrar)
-mostrar=calculadoraRectangulo(3,2.5)
-
