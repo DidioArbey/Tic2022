4d91b0d2af3d2a4357865a300a060b58c52ca6cf
Didio Perdomo <didioarbey@gmail.com>
2021-05-27 23:10:33 -0500

    ejercicios for part1

--- Semana3/ejercicios/for/9.py
+++ Semana3/ejercicios/for/9.py
@@ -1,3 +1,9 @@
-"""
-Desarrollar un programa que solicite la carga de 10 números e imprima la suma de los
-últimos 5 valores ingresados."""
+"""Desarrollar un programa que solicite la carga de 10 números e imprima la suma de los últimos 5 valores ingresados."""
+def yiyo():
+    lista=[]
+    for x in range(10):
+        valores=int(input(f"Ingrese el {x+1} numero: "))
+        lista.append(valores)
+    i=sum(lista[4:10])
+    print(i)
+yiyo()
